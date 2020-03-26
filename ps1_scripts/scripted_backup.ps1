c4b52e94a276ea9abc5562dfa5d56ab6c0d0086f
icedterminal <icedterminal@gmail.com>
2020-03-26 00:16:03 -0700

    new and updated scripts

---
+++ ps1_scripts/scripted_backup.ps1
@@ -0,0 +1,28 @@
+﻿$outputfile = "$env:systemdrive\PS_Output.log"
+Start-Transcript -path $outputfile -append
+
+Write-Host "
+Paths do not need to exist at this time, they will be created for you.
+Simply enter a root path where you want everything to end up
+A root path example is C:. If you want to use a hard drive mounted under F:, enter F:"
+
+$out = Read-Host 'Enter a root path'
+
+Write-Host "Backup files will reside at: $out\POWERSHELL_BACKUP"
+New-Item -Path "$out" -Name "POWERSHELL_BACKUP" -ItemType "directory"
+New-Item -Path "$out\POWERSHELL_BACKUP" -Name "PUBLIC" -ItemType "directory"
+New-Item -Path "$out\POWERSHELL_BACKUP" -Name "USER" -ItemType "directory"
+New-Item -Path "$out\POWERSHELL_BACKUP" -Name "PROGDATA" -ItemType "directory"
+
+takeown /f $env:ProgramData /a /r /d Y
+icacls $env:ProgramData /inheritance:r /t /q
+icacls $env:ProgramData /t /grant Administrators:F Users:F SYSTEM:F Everyone:F /q
+takeown /f $env:PUBLIC /a /r /d Y
+icacls $env:PUBLIC /inheritance:r /t /q
+icacls $env:PUBLIC /t /grant Administrators:F Users:F SYSTEM:F Everyone:F /q
+
+Copy-Item "$env:USERPROFILE\*" -Destination "$out\POWERSHELL_BACKUP\USER" -Recurse
+Copy-Item "$env:ProgramData\*" -Destination "$out\POWERSHELL_BACKUP\PROGDATA" -Recurse
+Copy-Item "$env:PUBLIC\*" -Destination "$out\POWERSHELL_BACKUP\PUBLIC" -Recurse 
+
+Stop-Transcript
--- ps1_scripts/wallpaper.ps1
+++ ps1_scripts/wallpaper.ps1
@@ -1,21 +1,23 @@
 ﻿Write-Host "
 
-Wallpaper images must be JPEG (.jpg) format. If it is not, please save it as such.
-In the space below, enter the name of your image. You must include the extension.
-EX: my-wallpaper.jpg
+Wallpaper images must be Portable Network Graphics (.png) format. If it is not, please convert.
+In the space below, enter the name of your image. DO NOT include the extension.
+Yes: my-wallpaper
+No: my-wallpaper.jpg
 
 " -ForegroundColor Yellow
 $mine = Read-Host 'Image name'
-copy-item "$env:USERPROFILE\Desktop\$mine" -destination "$env:APPDATA\Microsoft\Windows\Themes"
+copy-item "$env:USERPROFILE\Desktop\$mine.png" -destination "$env:APPDATA\Microsoft\Windows\Themes"
 cd $env:APPDATA\Microsoft\Windows\Themes
 remove-item TranscodedWallpaper
-rename-item $mine TranscodedWallpaper
+rename-item "$mine.png" TranscodedWallpaper
 copy-item TranscodedWallpaper -destination "CachedFiles"
 cd CachedFiles
 $name = Get-ChildItem -name *.jpg
 Remove-Item $name
 rename-item TranscodedWallpaper $name
 Write-Host "
-Right click on your Desktop and click Refresh
+Right click on your Desktop and click Refresh or log out / in.
 "
+#New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "JPEGImportQuality" -Value "100"  -PropertyType "DWORD"
 cd $env:USERPROFILE\Desktop
