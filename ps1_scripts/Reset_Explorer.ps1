e225b9261e7bc19339afc6204e9d7b4fef61f241
icedterminal <icedterminal@gmail.com>
2020-03-26 18:21:21 -0700

    Create Reset_Explorer.ps1

---
+++ ps1_scripts/Reset_Explorer.ps1
@@ -0,0 +1,18 @@
+﻿# Resets File Explorer and Taskbar.
+
+
+Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" -recurse -force
+Remove-Item -Path "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" -recurse -force
+Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\StreamMRU" -recurse -force
+Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Streams" -recurse -force
+Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\UserAssist" -recurse -force
+Remove-Item -Path "HKCU:\Software\Microsoft\Windows\Shell\BagMRU" -recurse -force
+Remove-Item -Path "HKCU:\Software\Microsoft\Windows\Shell\Bags" -recurse -force
+Remove-Item -Path "HKCU:\Software\Microsoft\Windows\ShellNoRoam\Bags" -recurse -force
+Remove-Item -Path "HKCU:\Software\Microsoft\Windows\ShellNoRoam\BagMRU" -recurse -force
+Remove-Item -Path "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\BagMRU" -recurse -force
+Remove-Item -Path "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\Bags" -recurse -force
+Remove-Item -Path "HKCU:\Software\Classes\Wow6432Node\Local Settings\Software\Microsoft\Windows\Shell\Bags" -recurse -force
+Remove-Item -Path "HKCU:\Software\Classes\Wow6432Node\Local Settings\Software\Microsoft\Windows\Shell\BagMRU" -recurse -force
+taskkill /f /im explorer.exe
+logoff
