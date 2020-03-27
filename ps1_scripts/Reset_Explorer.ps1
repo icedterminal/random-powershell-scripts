f5e9b6f41b776bcc1ac656395fb662a6ade7217a
icedterminal <icedterminal@gmail.com>
2020-03-27 04:24:56 -0700

    Update Reset_Explorer.ps1

--- ps1_scripts/Reset_Explorer.ps1
+++ ps1_scripts/Reset_Explorer.ps1
@@ -1,6 +1,6 @@
-﻿# Resets File Explorer and Taskbar.
+# Resets File Explorer and Taskbar.
 
-
+taskkill /f /im explorer.exe
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" -recurse -force
 Remove-Item -Path "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" -recurse -force
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\StreamMRU" -recurse -force
@@ -14,5 +14,4 @@ Remove-Item -Path "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Wind
 Remove-Item -Path "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\Bags" -recurse -force
 Remove-Item -Path "HKCU:\Software\Classes\Wow6432Node\Local Settings\Software\Microsoft\Windows\Shell\Bags" -recurse -force
 Remove-Item -Path "HKCU:\Software\Classes\Wow6432Node\Local Settings\Software\Microsoft\Windows\Shell\BagMRU" -recurse -force
-taskkill /f /im explorer.exe
 logoff
